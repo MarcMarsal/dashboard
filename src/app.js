dc1141bbf8ab18f57c61ff7a1bbf395e2948ab7c
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-03-13 10:55:44 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
 import express from "express";
 import cors from "cors";
 
+import backtestRoutes from "./api/backtest.routes.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Aquí afegirem les rutes del dashboard
-// Exemple:
-// import backtestRoutes from "./api/backtest.routes.js";
-// app.use("/backtest", backtestRoutes);
-
-import helloRoutes from "./api/hello.routes.js";
-app.use("/hello", helloRoutes);
-
+app.use("/backtest", backtestRoutes);
 
 export default app;
