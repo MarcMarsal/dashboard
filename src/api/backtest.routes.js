5b30ab60c10240f6fc25bdddd2eea0c1b433d753
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-03-16 11:01:53 +0100

    Update backtest.routes.js

--- src/api/backtest.routes.js
+++ src/api/backtest.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { runBacktest } from "../controllers/backtest.controller.js";
 import { getStats } from "../controllers/backtest.controller.js";
 import { fetchBacktestResults } from "../controllers/backtest.controller.js";
+import { fetchSegmentReport } from "../controllers/backtest.controller.js";
+
 
 const router = Router();
 
